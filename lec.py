91ae1c3da98ccd1363697d6524c1cad9571ab150
Margarita <alis-weasley@mail.ru>
2022-02-06 12:26:11 +0300

    Add gitignore, tasks

---
+++ lec.py
@@ -0,0 +1,88 @@
+# value = None
+
+# a = 123
+# b = 1.23
+# value = 12345
+# # print(type(a)) 
+# print(type(b)) 
+# print(type(value)) 
+
+# s = 'hello world'
+# print(s) # вывод строки
+    
+# print (a, '-',b, '-',s)
+# print ('{1} - {2} - {0}'.format(a, b, s))
+# print (f'{a} - {b} - {s}')
+
+# f = True
+# print (f)
+
+# Списки
+# list = ['1', '2', '3', 'hello world']
+# print (list)
+
+# Ввод и вывод данных
+# print ('Введите a')
+# a = int(input())
+# print ('Введите b')
+# b = int(input())
+# print (a, ' + ', b, ' = ', a+b)
+# # print (f'{a} {b}')
+
+# Арифметические операции
+# a = 1.3
+# b = 3
+# c = round(a*b,3)
+# print (c)
+
+# Сокращенные операции
+# a = 3
+# a += 5
+# print (a)
+
+# Логические опрации
+# a = 1 != 2
+# a = 'qwe'
+# b = 'qwe'
+# a = 1 < 3 < 5
+
+# func = 1
+# T = 4
+# x = 123
+
+# f = 2 < 3 or 4 > 6
+
+# f = [1, 2, 3, 4]
+# # print (not 2 in f)
+
+# is_odd = f[0] % 2 == 0
+# print (is_odd)
+
+# orig = 23
+# inv = 0
+
+# while orig != 0:
+#     inv = inv * 10 + (orig % 10)
+#     orig //= 10
+# else:
+#     print ('Пожалуй')
+#     print ('хватит ) ')
+# print (inv)
+
+# r = range(10)
+# # for i in range(1,10,2):
+# for i in 'qwerty':
+#     print (i)
+
+# Функции
+def f(x):
+    if x == 1:
+        return 'Целое'
+    elif x == 2.3:
+        return 23
+    else:
+        return
+
+arg = 2
+print (f(arg))
+print (type(f(arg)))
