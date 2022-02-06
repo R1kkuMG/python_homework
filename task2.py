24f6e9b38b5c91b390c04a6d9f63b1b8b533c575
Margarita <alis-weasley@mail.ru>
2022-02-06 13:04:23 +0300

    Add task2

---
+++ task2.py
@@ -0,0 +1,23 @@
+# 2. Найти максимальное из пяти чисел
+
+print ('Введите 1 число:')
+a = int(input())
+print ('Введите 2 число:')
+b = int(input())
+print ('Введите 3 число:')
+c = int(input())
+print ('Введите 4 число:')
+d = int(input())
+print ('Введите 5 число:')
+e = int(input())
+
+max = a
+if b > max:
+    max = b
+if c > max:
+    max = c
+if d > max:
+    max = d
+if e > max:
+    max = e
+print (f'Максимальное число = {max}')
