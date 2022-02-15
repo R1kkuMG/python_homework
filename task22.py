8d87949a2e6462398f8515be6c94fe6b72712d85
Margarita <alis-weasley@mail.ru>
2022-02-15 17:51:12 +0300

    Add: Найти сумму чисел списка стоящих на нечетной позиции

---
+++ task22.py
@@ -0,0 +1,9 @@
+# 22. Найти сумму чисел списка стоящих на нечетной позиции
+
+list = [1,3,5,7,9,11]
+summ = 0
+for i in range(len(list)): # чтобы i принимала значение индексов
+    if i % 2 != 0:
+        print (list[i])
+        summ = summ + list[i]
+print(summ)
