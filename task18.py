825cdc5bbc8be3631325c8a57eaa038b1de6c86c
Margarita <alis-weasley@mail.ru>
2022-02-17 10:59:15 +0300

    Update Реализовать алгоритм перемешивания списка.

--- task18.py
+++ task18.py
@@ -1,8 +1,19 @@
 # 18. Реализовать алгоритм перемешивания списка. 
 
-import random
+# первое решение
+# import random
 
-list = [1,2,3,4,5,6]
-random.shuffle(list)
+# list = [1,2,3,4,5,6]
+# random.shuffle(list)
  
-print(list)
+# print(list)
+
+
+# второе решение
+import random 
+first_list = list(range(9))
+for i in range(0,len(first_list)-1):
+    j = random.randint(0,len(first_list)-1)
+    first_list[i], first_list[j] = first_list[j], first_list[i]
+
+print (first_list)
