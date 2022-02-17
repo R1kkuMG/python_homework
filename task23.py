6b7d46f3b38bd76a67af805b06fadbda2fa71230
Margarita <alis-weasley@mail.ru>
2022-02-17 11:14:18 +0300

    Update: Найти произведение пар чисел в списке.

--- task23.py
+++ task23.py
@@ -2,10 +2,12 @@
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
 # Пример: [2, 3, 4, 5, 6] => [12, 15, 16]; [2, 3, 5, 6] => [12, 15]
 
-list = [2,3,4,5,6]
+list = [2,3,4,5,6,7]
+list2 = []
 mult = 0
-
-print (f'[{list[0]*list[4]}, {list[1]*list[3]}, {list[2]**2}]')
-# for i in range(len(list)): # чтобы i принимала значение индексов
-#     mult = list[i] * list [i - 1]
-#     print (mult)
+print(list)
+for i in range(len(list)):
+    mult = list[i]*list[len(list) - 1 - i]
+    if i < len(list)/2:
+        list2.append(mult)    
+print(list2)
