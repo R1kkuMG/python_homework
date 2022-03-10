8cb9f96c79b053634b4cd0bfcb3d1e13950a6f9a
Margarita <alis-weasley@mail.ru>
2022-03-10 19:24:07 +0300

    Add: Дана последовательность чисел. Получить список неповторяющихся элементов исходной последовательности

---
+++ task32.py
@@ -0,0 +1,15 @@
+# 32. Дана последовательность чисел. Получить список неповторяющихся элементов исходной последовательности
+#     Пример: [1, 2, 3, 5, 1, 5, 3, 10] => [1, 2, 3, 5, 10]
+
+my_list = [1, 3, 6, 3, 6, 12]
+new_list = []
+
+# def f_1(x):
+#     global new_list
+#     if x not in new_list:
+#         new_list.append(x)
+
+# f = lambda x: new_list.append(x) if x not in new_list else False
+
+res = list(filter(lambda x: new_list.append(x) if x not in new_list else False, my_list))
+print(new_list)
