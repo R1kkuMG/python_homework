92c336ed8de40d5247dfd4695665ab0aa67c7122
Margarita <alis-weasley@mail.ru>
2022-03-19 19:19:07 +0300

    update /calc

--- calc/controller.py
+++ calc/controller.py
@@ -1,9 +1,9 @@
-import model_mult as model
+import model_sub as model
 import view
 
 def button_click():
     value_a = view.get_value()
     value_b = view.get_value()
     model.init(value_a, value_b)
-    result = model.mult()
-    view.view_data(result)
+    result = model.do_it()
+    view.view_data(result, "result")
--- calc/view.py
+++ calc/view.py
@@ -1,5 +1,5 @@
-def view_data(data):
-    print(f'result = {data}')
+def view_data(data, title):
+    print(f'{title} = {data}')
 
 def get_value():
     return int(input('value = '))
